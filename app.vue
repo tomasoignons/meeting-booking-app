8c35befda4c3a1771f18dc32ac5c06d96c0a5fbe
tomasoignons <tomasoignons@gmail.com>
2025-10-26 02:31:11 +0200

    added snipet

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://plausible.omont.ch/js/script.js',
+      defer: true,
+      'data-domain': 'clastrix.omont.ch'
+    }
+  ]
+})
+</script>
